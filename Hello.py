11611e4a010727342a7a58877a295a57c1a50880
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-06-04 22:29:09 +0200

    Erste Tag
    Hallo Welt, Mathe, Variable

---
+++ Hello.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  4 22:12:25 2023
+
+@author: Asus
+"""
+#my first programm
+print("Hello World")
+vorname = "Shakhriddin"
+nachname = "Korakhanov"
+age = 26
+geschlechte = "Männlich"
+nat = "Usbekisch"
+print(vorname, nachname,age,geschlechte, nat)
+
+#Arifmetika
+a=15
+b=25
+c=(a**3)-(b**2)
+print(c)
+
+#calculate circle
+r=5
+pi=3.14
+s=((r^2)*pi)
+print(s)
+
+#
+x = int(input("Qiymatini kiritig: "))
+y = int(input("Paramaetrlarni kiriting: "))
+z = (x+y)**2
+print(z)
