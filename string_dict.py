80a150f009fb7bd023b626e59b00939b385eb456
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-06-05 22:56:49 +0200

    Jeden Tag Python #2
    String

---
+++ string_dict.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 22:30:07 2023
+
+@author: Asus
+"""
+
+vorname= "shakhin"
+nachname= "korakhanli"
+vollname = f"{vorname} {nachname}"
+#print(f"Mening ismim {vorname} familiyam esa {nachname}. Tanishganimdan xursandman")
+
+#Methode
+#print(vorname.lower()) # barcha harflarni kichik qiladi
+#print(nachname.upper()) #barcha harflarni katta qiladi
+#print(vollname.upper())
+
+#print(vollname.title()) # faqat so'z boshidagi harfni katta qiladi
+
+#obst="     Apfel      "
+#gemuse = "Kartofel     "
+#print("Men"+ obst.strip() +" " + "yaxshi ko'raman")
+
+ism =input("To'liq Ismingizni kiriting ?\n>>>")
+print("Assalom Alekum Xurmatli "+ism.title())
+
+
