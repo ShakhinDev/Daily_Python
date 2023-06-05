7bf2f08a1124454d08e5b65e64cb820cf7aa9903
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-06-05 23:09:09 +0200

    Jeden Tag Python #2

---
+++ for_oper.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  5 22:57:45 2023
+
+@author: Asus
+
+"""
+
+mevalar = ["olma",'anor','anjir', 'shaftoli', 'orik']
+sonlar = [1234,4567,7890,-45,-98, 23.5, 49.99]
+print(sonlar[1])
+sonlar[-1]
+sonlar[2]
+sonlar[5]
+sonlar[-2]
+friends=[]
