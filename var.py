72faee5c8cb821087095219fcd4684d6cb789f46
Shakhriddin Korakhanov <123419705+ShakhinDev@users.noreply.github.com>
2023-06-06 23:04:54 +0200

    Jeden Tag Python #3
    for skil, if elif else

---
+++ var.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+year= 25
+name = 'Shakhriddin'
+lname= 'Korakhanov'
+phone_number = "+49123456778"
+
+
+# In[2]:
+
+
+print(year,name,lname,phone_number)
+
+
+# In[4]:
+
+
+#Praktika
+hi="Hello World"
+print(hi)
+
+
+# In[5]:
+
+
+xabar="Ertaga darsga borishim kerak"
+print(xabar)
+
+
+# In[8]:
+
+
+radius= 5
+PI = 3.14
+aylana_yuzi= PI*radius**2
+
+
+# In[10]:
+
+
+print(aylana_yuzi)
+
+
+# In[12]:
+
+
+print(f"Radiusi {radius} ga teng bo'lgan aylananing yuzi {aylana_yuzi} ga teng bo'ladi")
+
+
+# In[ ]:
+
+
+
+
